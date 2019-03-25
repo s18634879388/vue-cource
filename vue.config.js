e4d3838d77a82f39a057b7a5ce656016ab5f5d58
shixq <shixq@si-tech.com.cn>
2019-03-25 16:47:35 +0800

    first commit 项目搭建

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/iview-admin/' : '/'
+const path = require('path')  //引用node的path模块
+// const resolve = dir => {
+//   return path.join(__dirname,dir)
+// }
+const resolve = dir => path.join(__dirname,dir)
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  baseUrl: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+    .set('@',resolve('src'))
+    .set('_c',resolve('src/components'))
+  },
+  productionSourceMap: false, //打包时不生成.map文件
+  // 跨域配置
+  devServer: {
+    proxy: 'localhost:4000'
+  }
 }
